c9fa8d92bc7ea16df71a68a2bdf14f2c08b8b608
ordinCode <ysh1346@naver.com>
2020-01-06 18:19:27 +0900

    Update GameManager.java

--- 숫자야구게임/src/domain/GameManager.java
+++ 숫자야구게임/src/domain/GameManager.java
@@ -7,28 +7,28 @@ public class GameManager {
     private Computer computer = new Computer();
     private User user = new User();
 
-    public void gameStart(){
+    public void gameStart() {
         do {
             setting();
             gaming();
         } while (Input.isEnd() == 2);
     }
 
-    public void setting(){
+    public void setting() {
         computer.setNumber();
         computer.getNumber();
     }
 
-    public void gaming(){
+    public void gaming() {
         user.setNumber();
-        while (compare()[1]!=3){
+        while (compare()[1] != 3) {
             user.setNumber();
         }
     }
 
-    public int[] compare(){
+    public int[] compare() {
         int[] ballStrike = new int[2];
-        ballStrike = Rule.ballStrike(computer.getNumber(),user.getNumber());
+        ballStrike = Rule.ballStrike(computer.getNumber(), user.getNumber());
         return ballStrike;
     }
 }
